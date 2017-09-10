91deac43434aab1551bbf408d9d9d5992e5fce21
SimonIsMe <szym1991@gmail.com>
2017-09-10 17:14:28 +0200

    Pushing and pulling messages to and from frontend-api server

--- src/app.js
+++ src/app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
 var amqp = require('amqp');
-
+var request = require('ajax-request');
 
 var frontendId = process.env.FRONTEND_ID;
 var queueHost = process.env.QUEUE_HOST;
 var queuePort = process.env.QUEUE_PORT;
-
+var frontendApiHost = process.env.FRONTEND_API_HOST;
+var frontendApiPort = process.env.FRONTEND_API_PORT;
 
 function getUniqueConnectionId() {
     return (new Date()).getTime() + '' + Math.round(Math.random() * 10000000000) + 'abc';
@@ -26,23 +27,25 @@ app.get('/', function(req, res, next){
 app.ws('/', function(ws, req) {
 
     req.connectionId = getUniqueConnectionId();
-    console.log('on connect', req.connectionId);
 
     connections[req.connectionId] = {
         req: req,
         ws: ws
     };
 
-    ws.on('message', function(msg) {
-        console.log('on message', req.connectionId);
-
-        for (var connectionId in connections) {
-            connections[connectionId].ws.send('Mamy ' + Object.keys(connections).length + ' klienów');
-        }
+    ws.on('message', function(message) {
+        var jsonMessage = JSON.parse(message);
+        jsonMessage.clientId = frontendId + '-' + req.connectionId;
+        request({
+            url: frontendApiHost + ':' + frontendApiPort,
+            method: 'POST',
+            data: jsonMessage
+        }, function(err, res, body) {
+            ws.send(body)
+        });
     });
 
     ws.on('close', function(msg) {
-        console.log('on close', req.connectionId);
         delete connections[req.connectionId];
     });
 });
